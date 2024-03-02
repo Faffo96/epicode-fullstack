cf367a7c3c8cb90383d202ab9dc999ba4d2cd931
Faffo96 <fabioscar96@gmail.com>
2024-03-02 18:11:06 +0100

    S6L5

---
+++ UNIT2/sett2/giorno5/esercizio/README.md
@@ -0,0 +1,31 @@
+ 
+# Esercizio NetFlix
+
+In quest'esercizio ho trasformato in codice cio' che veniva mostrato nei mockup. Ho fatto qualche aggiunta extra-traccia.
+
+## Tabella dei Contenuti
+
+1. [Introduzione](#introduzione)
+2. [Modifiche Extra](#modifiche-extra)
+3. [Contatti](#contatti)
+
+## Introduzione
+
+Il layout è stato replicato al meglio su tutte e 3 le pagine, cercando di rimanere il piu' fedele possibile ai mockups, in tutte le risoluzioni richieste.
+
+## Modifiche Extra
+
+Elenco delle principali modifche extra:
+
+1. index.html:
+1.1. Aggiunta di un bottone di accensione all'apertura di index.html.
+1.2 Tutti gli elementi cliccabili (dropdown compresi) hanno un effetto di hover.
+1.3 Le immagini del carousel hanno un effetto di hover.
+1.4 Il logo NetFlix ha un effetto di hover.
+1.5 I bottoni social del footer hanno un loro effetto di hover specifico.
+
+
+## Contatti
+
+Modi per contattare il team di sviluppo del progetto o il proprietario del repository, ad esempio tramite email, social media o forum di discussione.
+
--- UNIT2/sett2/giorno5/esercizio/assets/js/script.js
+++ UNIT2/sett2/giorno5/esercizio/assets/js/script.js
@@ -3,11 +3,12 @@ const documentImgs2 = document.getElementsByClassName('carousel2');
 const documentImgs3 = document.getElementsByClassName('carousel3');
 let randomNumbers = [];
 
-
+/* Funzione per generare un numero random */
 const generateRandomNumber = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   };
 
+  /* Funzione per popolare il carosello con immagini in un ordine random ma senza ripetizioni */
   const populateCarousel = (carouselArray, generalSrc) => {
     for (let index = 0; index < carouselArray.length; index++) {
         let randomNumber;
@@ -17,7 +18,7 @@ const generateRandomNumber = (min, max) => {
         do {
             randomNumber = generateRandomNumber(0, 10);
         } while (randomNumbers.includes(randomNumber)); // Continua a generare un nuovo numero finché non è univoco
-        randomNumbers.push(randomNumber); // Aggiungi il numero univoco all'array
+        randomNumbers.push(randomNumber);
         carouselArray[index].setAttribute('src', `${generalSrc}${randomNumber}.jpg`);
     }
 }
@@ -27,16 +28,39 @@ populateCarousel(documentImgs2, 'assets/media/watchItAgain/media')
 populateCarousel(documentImgs3, 'assets/media/newReleases/media')
 
 
-document.addEventListener('DOMContentLoaded', function() {
+// Funzione per aprire il modal al caricamento della pagina
+function openModalOnLoad() {
     var myModal = new bootstrap.Modal(document.getElementById('exampleModal'));
     myModal.show();
-  });
+}
 
-  function addFadeClassToModal() {
+// Aggiunta della classe di dissolvenza al modal dopo un leggero ritardo, in modo che venga applicata la classe
+function addFadeClassToModal() {
     const modal = document.getElementById('exampleModal');
     if (modal.classList.contains('show')) {
         modal.classList.add('fade');
     }
 }
 
+document.addEventListener('DOMContentLoaded', openModalOnLoad);
 setTimeout(addFadeClassToModal, 100);
+
+
+/* AUDIO INTRO */
+
+let checkbox = document.getElementById("checkbox");
+let audio = new Audio();
+audio.src = "./netflix-opening-intro-4k.mp3";
+
+// Gestione dell'evento "canplaythrough" per assicurarsi che l'audio sia caricato prima di riprodurlo
+audio.addEventListener("canplaythrough", function() {
+    console.log("Audio caricato correttamente");
+    checkbox.addEventListener("change", function() {
+        if (checkbox.checked) {
+            audio.play();
+        } else {
+            audio.pause(); 
+        }
+    });
+});
+
