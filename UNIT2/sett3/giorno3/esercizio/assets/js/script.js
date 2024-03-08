adeabfe99f266a02b8f8bf2904c831158aae31cb
Faffo96 <fabioscar96@gmail.com>
2024-03-08 09:11:23 +0100

    S7L4

--- UNIT2/sett3/giorno3/esercizio/assets/js/script.js
+++ UNIT2/sett3/giorno3/esercizio/assets/js/script.js
@@ -1,62 +1,140 @@
 let libri;
 let cart = [];
 
-fetch('https://striveschool-api.herokuapp.com/books')
-    .then(response => {
-        return response.json();
-    })
-    .then(data => {
-        libri = data;
-        console.log(libri)
-        fillCards(libri)
-    })
-    .catch(err => {
-        console.log('Mio Errore');
-    })
 
 
-    function fillCards(arrayLibri) {
-        const documentCardImg = document.getElementsByClassName('card-img-top');
-        const documentCardTitle = document.getElementsByClassName('card-title');
-        const documentCardCategory = document.getElementsByClassName('card-category');
-        const documentCardPrice = document.getElementsByClassName('card-price');
+addEventListener('load', init())
 
-        
-        for (let i = 0; i < arrayLibri.length; i++) {
-            const element = arrayLibri[i];
-            documentCardImg[i].setAttribute('src', `${element.img}`);
-            documentCardTitle[i].innerText = `${element.title}`;
-            documentCardCategory[i].innerText = `${element.category}`;
-            documentCardPrice[i].innerText = `${element.price}`;
-        }
+function init() {
+    fetch('https://striveschool-api.herokuapp.com/books')
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            libri = data;
+            console.log(libri)
+            fillCards(libri);
+            addToCart();
+        })
+        .catch(err => {
+            console.log('Mio Errore');
+        })
+}
+
+function fillCards(arrayLibri) {
+    const bookList = document.querySelector('.bookList');
+
+    for (let i = 0; i < arrayLibri.length; i++) {
+        const element = arrayLibri[i];
+
+        // Creazione della card
+        const cardDiv = document.createElement('div');
+        cardDiv.classList.add('card');
+        cardDiv.setAttribute("id", element.asin);
+
+        const img = document.createElement('img');
+        img.classList.add('card-img-top');
+        img.setAttribute('src', element.img);
+        img.setAttribute('alt', '...');
+        cardDiv.appendChild(img);
+
+        const cardBody = document.createElement('div');
+        cardBody.classList.add('card-body');
+        cardDiv.appendChild(cardBody);
+
+        const title = document.createElement('h5');
+        title.classList.add('card-title');
+        title.innerText = element.title;
+        cardBody.appendChild(title);
+
+        const category = document.createElement('p');
+        category.classList.add('card-category');
+        category.innerText = element.category;
+        cardBody.appendChild(category);
+
+        const price = document.createElement('p');
+        price.classList.add('card-price');
+        price.innerText = element.price + '€';
+        cardBody.appendChild(price);
+
+        const compraBtn = document.createElement('a');
+        compraBtn.classList.add('card-buy', 'btn', 'btn-danger');
+        compraBtn.setAttribute('href', '#');
+        compraBtn.innerText = 'Compra Ora';
+        cardBody.appendChild(compraBtn);
+
+        const scartaBtn = document.createElement('a');
+        scartaBtn.classList.add('btn', 'btn-outline-danger');
+        scartaBtn.setAttribute('href', '#');
+        scartaBtn.innerText = 'Scarta';
+        cardBody.appendChild(scartaBtn);
+
+        bookList.appendChild(cardDiv);
     }
+}
 
+
+function addToCart() {
     const documentBuy = document.getElementsByClassName('card-buy');
     for (let i = 0; i < documentBuy.length; i++) {
         const element = documentBuy[i];
         element.addEventListener('click', () => {
             const selectedBook = libri[i];
-            console.log(selectedBook)
+            console.log(selectedBook);
             cart.push(selectedBook);
             console.log('Elemento aggiunto al carrello:', selectedBook);
-            
-            const documentCartCardBody = document.getElementsByClassName('modal-body');
-            let newDiv = document.createElement('div');
-            newDiv.setAttribute('class', 'cart-card container-fluid');
-            let newDivRow = document.createElement('div');
-            newDivRow.setAttribute('class', 'row');
-            let newImg = document.createElement('img');
-            newImg.setAttribute('src', `${element.img}`);
-            let newDivCardInfo = document.createElement('div');
-            newDivCardInfo.setAttribute('class', '.cart-card-info col-6')
-            let newInfoP = document.createElement('p');
-            newInfoP.setAttribute('class', 'cart-card-info-title');
-            let newInfoB = document.createElement('b');
-            newInfoB.setAttribute('class', 'cart-card-info-price');
-            
-    })
+
+            const documentModalBody = document.querySelector('.modal-body');
+
+            // Creazione della card del carrello
+            const newDiv = document.createElement('div');
+            newDiv.classList.add('cart-card', 'container-fluid');
+            newDiv.setAttribute('id', `${selectedBook.asin}`);
+
+            const newDivRow = document.createElement('div');
+            newDivRow.classList.add('row');
+
+            const newImg = document.createElement('img');
+            newImg.setAttribute('src', `${selectedBook.img}`);
+            newImg.classList.add('imgSize');
+
+            const newDivCardInfo = document.createElement('div');
+            newDivCardInfo.classList.add('cart-card-info', 'col-6');
+
+            const newInfoP = document.createElement('p');
+            newInfoP.classList.add('cart-card-info-title');
+            newInfoP.innerText = selectedBook.title;
+
+            const newInfoB = document.createElement('b');
+            newInfoB.classList.add('cart-card-info-price');
+            newInfoB.innerText = `${selectedBook.price}€`;
+
+            const newDivCartCardBtn = document.createElement('div');
+            newDivCartCardBtn.classList.add('cart-card-info-button', 'col-3');
+
+            const newButton = document.createElement('button');
+            newButton.classList.add('cart-card-info-delete', 'btn', 'btn-danger');
+
+            const newIcon = document.createElement('i');
+            newIcon.classList.add('bi', 'bi-trash3');
+
+            newButton.appendChild(newIcon);
+            newDivCardInfo.appendChild(newInfoP);
+            newDivCardInfo.appendChild(newInfoB);
+            newDivRow.appendChild(newImg);
+            newDivRow.appendChild(newDivCardInfo);
+            newDivRow.appendChild(newDivCartCardBtn);
+            newDivCartCardBtn.appendChild(newButton);
+            newDiv.appendChild(newDivRow);
+            documentModalBody.appendChild(newDiv);
+        });
     }
-    
+}
+
+
+
+
+
 
 /* {
     0:
---
+++ UNIT2/sett3/giorno4/esercizio/assets/js/script.js
@@ -0,0 +1,79 @@
+let documentButton1 = document.querySelector('.btn1');
+let documentButton2 = document.querySelector('.btn2');
+let documentButton3 = document.querySelector('.btn3');
+const viewButtons = document.querySelectorAll('.btnView');
+let documentModalImg = document.querySelector('.modalImg')
+
+let endPoint = 'https://api.pexels.com/v1/search?query=';
+
+addEventListener("load", init);
+
+function init() {
+    loadLastSearch();
+}
+
+function loadLastSearch() {
+    const lastSearch = localStorage.getItem('lastSearch');
+    if (lastSearch) {
+        loadImg(lastSearch);
+    }
+}
+
+viewButtons.forEach(button => {
+    button.addEventListener('click', function () {
+        // Trova l'immagine associata alla card padre del bottone cliccato
+        const card = this.closest('.card');
+        const img = card.querySelector('.card-img-top');
+
+        // Ottieni l'URL dell'immagine e stampalo nella console
+        const src = img.getAttribute('src');
+
+        setModalImg(src);
+    })
+})
+
+function setModalImg(src) {
+    documentModalImg.setAttribute('src', `${src}`);
+    documentModalImg.setAttribute('width', '450px');
+}
+
+async function loadImg(inputUtente) {
+    try {
+        const response = await fetch(endPoint + inputUtente, {
+            method: 'GET',
+            headers: {
+                Authorization: "Bearer TXAl4tsq0AH939vI13rKvpyMo7xSgfkSwtTUiEnLKjjvEI0zqABAVoL5"
+            }
+        });
+        if (!response.ok) {
+            throw new Error('Errore nella richiesta');
+        }
+        const fetchResult = await response.json();
+        let documentImgs = document.getElementsByClassName('bd-placeholder-img card-img-top');
+        for (let i = 0; i < documentImgs.length; i++) {
+            const element = documentImgs[i];
+            element.setAttribute('src', `${fetchResult.photos[i].src.original}`);
+        }
+
+        // Salvataggio delle ultime immagini mostrate nel local storage
+        localStorage.setItem('lastImages', JSON.stringify(fetchResult.photos.map(photo => photo.src.original)));
+    } catch (error) {
+        console.error('Errore durante la richiesta:', error);
+    }
+}
+
+documentButton1.addEventListener("click", () => {
+    loadImg('mountains');
+    localStorage.setItem('lastSearch', 'mountains');
+});
+
+documentButton2.addEventListener("click", () => {
+    loadImg('sunsets');
+    localStorage.setItem('lastSearch', 'sunsets');
+});
+
+documentButton3.addEventListener("click", () => {
+    let valueUtente = document.getElementById('valueUtente').value;
+    loadImg(valueUtente);
+    localStorage.setItem('lastSearch', valueUtente);
+});
