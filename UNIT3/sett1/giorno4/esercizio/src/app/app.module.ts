0ac48cdce31b76ba6515e2046a98b2e0bae32008
Faffo96 <fabioscar96@gmail.com>
2024-03-21 15:04:32 +0100

    S9L4

---
+++ UNIT3/sett1/giorno4/esercizio/src/app/app.module.ts
@@ -0,0 +1,56 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { Route, RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MainComponent } from './components/main/main.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { HomeSection1JumbotronComponent } from './components/home-section1-jumbotron/home-section1-jumbotron.component';
+import { HomeSection21CardsCardComponent } from './components/home-section2-1-cards-card/home-section2-1-cards-card.component';
+import { HomeSection1CardsCardComponent } from './components/home-section1-cards-card/home-section1-cards-card.component';
+import { ActivePostsComponent } from './components/active-posts/active-posts.component';
+import { UnactivePostsComponent } from './components/unactive-posts/unactive-posts.component';
+
+const routes: Route[] = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'activePosts',
+    component: ActivePostsComponent
+  },
+  {
+    path: 'unactivePosts',
+    component: UnactivePostsComponent
+  },
+  {
+    path: '**',
+    redirectTo:''
+  }
+]
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    FooterComponent,
+    HomeComponent,
+    HomeSection1JumbotronComponent,
+    HomeSection21CardsCardComponent,
+    HomeSection1CardsCardComponent,
+    ActivePostsComponent,
+    UnactivePostsComponent,
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ UNIT3/sett1/giorno4/esercizio/src/app/components/unactive-posts/unactive-posts.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-unactive-posts',
+  templateUrl: './unactive-posts.component.html',
+  styleUrls: ['./unactive-posts.component.scss']
+})
+export class UnactivePostsComponent {
+
+}
