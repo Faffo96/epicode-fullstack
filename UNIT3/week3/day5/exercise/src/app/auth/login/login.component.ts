e2cbd42e9f9d380f1ba821f7128e544c9aded224
Faffo96 <fabioscar96@gmail.com>
2024-04-08 00:41:38 +0200

    S11L5 - First login attempt fail fixed

--- UNIT3/week3/day5/exercise/src/app/auth/login/login.component.ts
+++ UNIT3/week3/day5/exercise/src/app/auth/login/login.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
-
+import { AuthData } from 'src/app/models/auth-data.interface';
 
 @Component({
     selector: 'app-login',
@@ -10,13 +10,20 @@ import { AuthService } from '../auth.service';
     styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
-    constructor(private authSrv: AuthService, private router: Router) {}
+    user!: AuthData | null;
+
+    constructor(private authService: AuthService, private router: Router) {
+        this.authService.user$.subscribe((user) => {
+            this.user = user;
+        });
+    }
 
     login(form: NgForm) {
-        console.log(form.value);
         try {
-            this.authSrv.login(form.value).subscribe();
-            this.router.navigate(['/']);
+            this.authService.login(form.value).subscribe();
+            setTimeout(() => {
+                this.router.navigate(['/']);
+            }, 100);
         } catch (error) {
             console.error(error);
         }
