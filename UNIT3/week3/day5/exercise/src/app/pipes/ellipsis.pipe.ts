5d4c818e5633862897f2a1d663b2aea89c4ca24d
Faffo96 <fabioscar96@gmail.com>
2024-04-10 01:12:56 +0200

    router link click conflict with heart click - fixed

--- UNIT3/week3/day5/exercise/src/app/pipes/ellipsis.pipe.ts
+++ UNIT3/week3/day5/exercise/src/app/pipes/ellipsis.pipe.ts
@@ -5,10 +5,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class EllipsisPipe implements PipeTransform {
     transform(value: string): string {
-        if (value.length <= 18) {
+        if (value.length <= 30) {
             return value;
         } else {
-            return value.slice(0, 18) + '...';
+            return value.slice(0, 30) + '...';
         }
     }
 }
