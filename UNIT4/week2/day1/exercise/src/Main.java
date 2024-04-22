cca6898496975cf615f53e4d15b91b5f2762ad60
Faffo96 <fabioscar96@gmail.com>
2024-04-22 17:56:00 +0200

    S14L1

---
+++ UNIT4/week2/day1/exercise/src/Main.java
@@ -0,0 +1,60 @@
+import ArrayRandom.Random10;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Scanner;
+
+public class Main {
+    static Logger logger = LoggerFactory.getLogger("logger1");
+
+    // TRACE<DEBUG<INFO<WARN<ERROR
+    public static void main(String[] args) {
+        logger.info("Launching app..");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Your random numbers:");
+
+        Random10 a1 = new Random10();
+
+        for (int i = 0; i < a1.getArray().length; i++) {
+            System.out.println(a1.getArray()[i]);
+        }
+
+        System.out.println("Do you want to change one? (Y/N)");
+
+        String response = scanner.nextLine();
+        if (response.equalsIgnoreCase("Y")) {
+            int newNumber;
+            do {
+                System.out.println("Which position do you want change? (0-4)");
+                int i = scanner.nextInt();
+                scanner.nextLine();
+                System.out.println("Insert the new number: (0 will end the program.)");
+                newNumber = scanner.nextInt();
+                scanner.nextLine();
+                if (newNumber != 0) {
+                    a1.replaceArrayI(i, newNumber);
+
+                    System.out.println("Do you want to change an other one? (Y/N)");
+                    response = scanner.nextLine();
+                    if (response.equalsIgnoreCase("N")) {
+                        newNumber = 0;
+                    }
+                }
+
+
+            } while (newNumber != 0);
+        } else {
+            System.out.println("No changes made.");
+        }
+        System.out.println("Your new random numbers:");
+
+        for (int i = 0; i < a1.getArray().length; i++) {
+            System.out.println(a1.getArray()[i]);
+        }
+
+
+        scanner.close();
+        logger.info("Closing app..");
+    }
+}
